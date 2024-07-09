c2d2b1173fd7d2e307779938a1ef68aefe3e6f7e
asare4u <nkansahasare3@gmail.com>
2024-07-09 22:48:12 +0000

    Add Login Forms App

---
+++ LoginFormsApp/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace LoginFormsApp
+{
+    partial class Form1
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        
+        private void InitializeComponent()
+        {
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.userNameBox = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.passwordBox.Location = new System.Drawing.Point(132, 161);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.PasswordChar = '*';
+            this.passwordBox.Size = new System.Drawing.Size(281, 26);
+            this.passwordBox.TabIndex = 2;
+            this.passwordBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.loginButton.Location = new System.Drawing.Point(132, 208);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(202, 38);
+            this.loginButton.TabIndex = 3;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // userNameBox
+            // 
+            this.userNameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.userNameBox.Location = new System.Drawing.Point(132, 110);
+            this.userNameBox.Margin = new System.Windows.Forms.Padding(5);
+            this.userNameBox.Name = "userNameBox";
+            this.userNameBox.Size = new System.Drawing.Size(281, 26);
+            this.userNameBox.TabIndex = 1;
+            this.userNameBox.Tag = "";
+            this.userNameBox.TextChanged += new System.EventHandler(this.userNameBox_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.userNameBox);
+            this.panel1.Controls.Add(this.loginButton);
+            this.panel1.Controls.Add(this.passwordBox);
+            this.panel1.Location = new System.Drawing.Point(173, 74);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(468, 330);
+            this.panel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(26, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(28, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.label3.Location = new System.Drawing.Point(126, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(187, 38);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Login";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LoginScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(768, 511);
+            this.Controls.Add(this.panel1);
+            this.Name = "LoginScreen";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox userNameBox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ LoginFormsApp/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LoginFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void userNameBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+
+            string username = userNameBox.Text;
+            string password = passwordBox.Text;
+
+            MessageBox.Show($"Username : {username}\nPassword : {password}");
+        }
+
+    }
+}
